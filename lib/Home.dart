fc3bce4290d64c07870164305c8e8501c06386a5
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-05-03 12:11:46 +0100

    Testing the two methods insertData and getData in the front

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,4 @@
+import 'package:films/SQLDB.dart';
 import 'package:flutter/material.dart';
 
 
@@ -10,11 +11,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  SQLdb sqLdb = SQLdb();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title:Text("Home") ,),
-      body: Center(child: Text("Page Home"),),
+      body: Container(
+        margin: EdgeInsets.all(15),
+        child:  Column(
+          children: [
+            ElevatedButton(
+                onPressed: ()async{
+                  int rep =await sqLdb.insertData("INSERT INTO 'films'(title,duration) VALUES ('Hdidane','90')");
+                    print("${rep}");
+            },
+                child: Text("Insert")),
+            ElevatedButton(
+                onPressed: ()async{
+                  List<Map> films = await sqLdb.getData("SELECT * FROM 'films'");
+                  print("${films}");
+                },
+                child: Text("Display")),
+
+          ],
+        ),
+        
+    ),
     );
   }
 }
