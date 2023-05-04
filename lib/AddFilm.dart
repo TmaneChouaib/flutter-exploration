182c73080e65f14c8333015d551710e8a1a6907b
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-05-04 13:57:54 +0100

    Completing the user interface and the backend functionality for the AddFilm page

--- lib/AddFilm.dart
+++ lib/AddFilm.dart
@@ -1,3 +1,5 @@
+import 'package:films/Home.dart';
+import 'package:films/SQLDB.dart';
 import 'package:flutter/material.dart';
 
 class AddFilm extends StatefulWidget {
@@ -8,6 +10,9 @@ class AddFilm extends StatefulWidget {
 }
 
 class _AddFilmState extends State<AddFilm> {
+  TextEditingController title = TextEditingController();
+  TextEditingController duration= TextEditingController();
+  SQLdb sqLdb=SQLdb();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +21,34 @@ class _AddFilmState extends State<AddFilm> {
         margin: EdgeInsets.all(10),
         child: Column(
           children: [
-
+            SizedBox(height: 20,),
+            TextField(
+              controller: title,
+              style: TextStyle(fontSize: 20,color: Colors.purple),
+              decoration: InputDecoration(
+                labelText:"Title",
+                border:OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(40))),
+              ),
+             ),
+            SizedBox(height: 20,),
+            TextField(
+              controller: duration,
+              style: TextStyle(fontSize: 20,color: Colors.purple),
+              decoration: InputDecoration(
+                labelText:"duration",
+                border:OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(40))),
+              ),
+            ),
+            SizedBox(height: 20,),
+              ElevatedButton(
+                  onPressed:() async{
+                    int rep = await sqLdb.insertData("INSERT INTO 'films' (title,duration) VALUES('${title.text}',${int.parse(duration.text)})");
+                  if (rep>0)
+                    Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(builder: (context)=>Home()),
+                            (route) => false);
+                    },
+                  child: Container(child: Icon(Icons.add,size:30,),width:double.infinity,))
           ],
         ),
       ),
